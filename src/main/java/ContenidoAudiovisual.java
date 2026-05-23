d8762912c03b6e59d5525e578ce6e270f5dfdcfe
alexa <alexa@Alexander99G>
2026-05-23 16:48:13 -0500

    Actualizacion de la clase main para Tarea U4

--- src/main/java/ContenidoAudiovisual.java
+++ src/main/java/ContenidoAudiovisual.java
@@ -1,51 +1,64 @@
 package main.java;
 
 public abstract class ContenidoAudiovisual {
-	private static int contar = 0;
-	private String titulo;
-	private int duracionEnMinutos;
-	private String genero;
-	private int id;
+    private static int contador = 1; // Iniciamos en 1 por convención de IDs
+    private int id;
+    private String titulo;
+    private int duracionEnMinutos;
+    private String genero;
 
-	public ContenidoAudiovisual(String titulo, int duracionEnMinutos, String genero) {
-		this.id = contar++;
-		this.titulo = titulo;
-		this.duracionEnMinutos = duracionEnMinutos;
-		this.genero = genero;
-	}
+    // Constructor 1: Para crear un NUEVO contenido (ej. desde la consola)
+    public ContenidoAudiovisual(String titulo, int duracionEnMinutos, String genero) {
+        this.id = contador++;
+        this.titulo = titulo;
+        this.duracionEnMinutos = duracionEnMinutos;
+        this.genero = genero;
+    }
 
-	// Getter y Setter para el campo 'titulo'
-	public String getTitulo() {
-		return titulo;
-	}
+    // Constructor 2: Para cargar contenido EXISTENTE (desde el archivo CSV)
+    public ContenidoAudiovisual(int id, String titulo, int duracionEnMinutos, String genero) {
+        this.id = id;
+        this.titulo = titulo;
+        this.duracionEnMinutos = duracionEnMinutos;
+        this.genero = genero;
+        // Sincronizamos el contador para que los nuevos registros no repitan IDs
+        if (id >= contador) {
+            contador = id + 1;
+        }
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public int getId() {
+        return id;
+    }
 
-	// Getter y Setter para el campo 'duracionEnMinutos'
-	public int getDuracionEnMinutos() {
-		return duracionEnMinutos;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public void setDuracionEnMinutos(int duracionEnMinutos) {
-		this.duracionEnMinutos = duracionEnMinutos;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	// Getter y Setter para el campo 'genero'
-	public String getGenero() {
-		return genero;
-	}
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
 
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
 
-	// Getter para el campo 'id' (no se proporciona el Setter ya que 'id' se asigna
-	// en el constructor y parece ser inmutable)
-	public int getId() {
-		return id;
-	}
+    public String getGenero() {
+        return genero;
+    }
 
-	public abstract void mostrarDetalles();
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public abstract void mostrarDetalles();
+
+    // Método de ayuda para la Etapa 1: Formatea los datos para escribirlos en el archivo CSV
+    public String toCSV() {
+        return id + "," + titulo + "," + duracionEnMinutos + "," + genero;
+    }
 }
